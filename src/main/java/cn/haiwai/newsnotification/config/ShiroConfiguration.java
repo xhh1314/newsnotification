a14296100cda172f5f9edb3a5255bad7a0e7d18a
雕兄 <903440799@qq.com>
2017-12-25 20:01:17 +0800

    d

--- src/main/java/cn/haiwai/newsnotification/config/ShiroConfiguration.java
+++ src/main/java/cn/haiwai/newsnotification/config/ShiroConfiguration.java
@@ -1,6 +1,8 @@
 package cn.haiwai.newsnotification.config;
 
 import cn.haiwai.newsnotification.service.MyShiroRealm;
+import org.apache.shiro.authc.credential.CredentialsMatcher;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.cache.ehcache.EhCacheManager;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
@@ -46,16 +48,17 @@ public class ShiroConfiguration {
      * @author SHANHY
      * @create 2016年1月13日
      */
-//  @Bean
-//  public FilterRegistrationBean filterRegistrationBean() {
-//      FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
-//      filterRegistration.setFilter(new DelegatingFilterProxy("shiroFilter"));
-//      //  该值缺省为false,表示生命周期由SpringApplicationContext管理,设置为true则表示由ServletContainer管理
-//      filterRegistration.addInitParameter("targetFilterLifecycle", "true");
-//      filterRegistration.setEnabled(true);
-//      filterRegistration.addUrlPatterns("/*");// 可以自己灵活的定义很多，避免一些根本不需要被Shiro处理的请求被包含进来
-//      return filterRegistration;
-//  }
+
+ /*   @Bean
+ public FilterRegistrationBean filterRegistrationBean() {
+      FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
+      filterRegistration.setFilter(new DelegatingFilterProxy("shiroFilter"));
+      //  该值缺省为false,表示生命周期由SpringApplicationContext管理,设置为true则表示由ServletContainer管理
+      filterRegistration.addInitParameter("targetFilterLifecycle", "true");
+     filterRegistration.setEnabled(true);
+     filterRegistration.addUrlPatterns("/*");// 可以自己灵活的定义很多，避免一些根本不需要被Shiro处理的请求被包含进来
+      return filterRegistration;
+  }*/
     @Bean(name = "lifecycleBeanPostProcessor")
     public LifecycleBeanPostProcessor getLifecycleBeanPostProcessor() {
         return new LifecycleBeanPostProcessor();
@@ -68,6 +71,13 @@ public class ShiroConfiguration {
         return daap;
     }
 
+    //配置自定义的密码比较器
+    @Bean(name="credentialsMatcher")
+    public CredentialsMatcher credentialsMatcher() {
+        HashedCredentialsMatcher matcher=new HashedCredentialsMatcher();
+        matcher.setHashAlgorithmName("MD5");
+        return matcher;
+    }
     @Bean(name = "securityManager")
     public DefaultWebSecurityManager getDefaultWebSecurityManager(MyShiroRealm myShiroRealm) {
         DefaultWebSecurityManager dwsm = new DefaultWebSecurityManager();
